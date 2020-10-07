2decd8ccdabde475c2b256c0c8dcf7c5fe775899
Skyler R <skyler.rencher@gmail.com>
2020-10-06 21:58:47 -0400

    installed the inquirer npm. added some code to run some of the user prompts. Also added some feedback into the generated  markdown.

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,20 @@
+const inquirer = require("inquirer")
+const generateMarkdown = require("./utils/generateMarkdown")
+// require fs npm
+
 // array of questions for user
 const questions = [
+    {
+        message: "What is your name?",
+        name: "name"
 
+    }
+    ,
+    {
+        message: "What is your project title?",
+        name: "title"
+
+    }
 ];
 
 // function to write README file
@@ -9,6 +23,18 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    // Ask user questions about the project
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+            const markDown = generateMarkdown(answers);
+            console.log(markDown);
+        })
+    console.log("After questions!")
+    
+
+    // save string to a .md file
+    // fs.appendFile()
 
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,27 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `# ${answers.title}
 
+  [![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://nodesource.com/products/nsolid)
+
+  [![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+  
+  ${answers.name} is a cloud-enabled, mobile-ready, offline-storage, AngularJS powered HTML5 Markdown editor.
+  
+    - Type some Markdown on the left
+    - See HTML in the right
+    - Magic
+  
+  # New Features!
+  
+    - Import a HTML file and watch it magically convert to Markdown
+    - Drag and drop images (requires your Dropbox account be linked)
+  
+  
+  You can also:
+    - Import and save files from GitHub, Dropbox, Google Drive and One Drive
+    - Drag and drop markdown and HTML files into Dillinger
+    - Export documents as Markdown, HTML and PDF
 `;
 }
 
